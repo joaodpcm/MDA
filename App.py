6c81421ba9d6605133a83306ad9e913c06ad37c3
Joao Duarte <joao.dpcmarques@gmail.com>
2023-05-31 17:06:15 +0200

    update packages app

--- App.py
+++ App.py
@@ -10,13 +10,13 @@ import pickle
 from datetime import datetime
 from datetime import timedelta
 import sklearn
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.exceptions import NotFittedError
-from sklearn.inspection import permutation_importance
+# from sklearn.compose import ColumnTransformer
+# from sklearn.pipeline import Pipeline
+# from sklearn.preprocessing import StandardScaler, OneHotEncoder
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.metrics import accuracy_score
+# from sklearn.exceptions import NotFittedError
+# from sklearn.inspection import permutation_importance
 import requests
 import numpy as np
 import itertools as it
