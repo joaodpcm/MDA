85a57a81042f5442a6ef5c405d18c029c00c7b3e
Joao Duarte <joao.dpcmarques@gmail.com>
2023-05-31 19:29:05 +0200

    up app, noise... trial

--- App.py
+++ App.py
@@ -10,13 +10,14 @@ import pickle
 from datetime import datetime
 from datetime import timedelta
 import sklearn
-# from sklearn.compose import ColumnTransformer
-# from sklearn.pipeline import Pipeline
-# from sklearn.preprocessing import StandardScaler, OneHotEncoder
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import accuracy_score
-# from sklearn.exceptions import NotFittedError
-# from sklearn.inspection import permutation_importance
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.exceptions import NotFittedError
+from sklearn.inspection import permutation_importance
 import requests
 import numpy as np
 import itertools as it
@@ -27,9 +28,28 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.ensemble import HistGradientBoostingRegressor
 from sklearn.model_selection import LeaveOneGroupOut
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import accuracy_score
+from sklearn.exceptions import NotFittedError
+from sklearn.inspection import permutation_importance
+from xgboost import XGBClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.inspection import permutation_importance
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.model_selection import LeaveOneGroupOut
+from sklearn.exceptions import NotFittedError
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.experimental import enable_hist_gradient_boosting
 
-
-print(sklearn.__version__)
 
 #importing models
 url_class = 'https://api.github.com/repos/joaodpcm/MDA/contents/classifier.pkl'
@@ -50,29 +70,12 @@ hgr = pickle.loads(decoded_content_reg)
 
 
 
-# url_class = 'https://raw.githubusercontent.com/joaodpcm/MDA/master/classifier.pkl'
-# response_class = requests.get(url_class)
-# rfc = pickle.load(open('classifier.pkl','rb'))
 
-# url_reg= 'https://raw.githubusercontent.com/joaodpcm/MDA/master/regressor.pkl'
-# response_reg= requests.get(url_reg)
-# hgr = pickle.load(open('regressor.pkl','rb'))
-
-# with open('classifier.pkl', 'wb') as f:
-#     f.write(response_class.content)
-
-# with open('classifier.pkl', 'rb') as f:
-#     rfc = pickle.load(f)
-
-# with open('regressor.pkl','wb') as f:
-#     f.write(response_reg.content)
-
-# with open('regressor.pkl', 'rb') as f:
-#     hgr = pickle.load(f)
 
 
 #importing avarage of the noise
-url_hourly_avg = 'https://api.github.com/repos/joaodpcm/MDA/contents/hourly_acg_noise.csv'
+
+url_hourly_avg = 'https://raw.githubusercontent.com/joaodpcm/MDA/master/hourly_acg_noise.csv'
 
 df_hourly_avg=pd.read_csv(url_hourly_avg)
 
@@ -100,11 +103,10 @@ for i in range(len(time_range)):
 ].AverageNoise.values[0]
 
 #getting data for amount of events
-events_url = 'https://api.github.com/repos/joaodpcm/MDA/contents/shaped_filter_tags_city2_EXAM.csv'
-events=pd.read_csv(events_url)
+events_url = 'https://raw.githubusercontent.com/joaodpcm/MDA/master/shaped_filter_tags_city2_EXAM.csv'
+events=pd.read_csv(events_url,sep='\t')
 events['Time'] = pd.to_datetime(events['Time'])
 events_48 = events[events['Time'].isin(time_range)]
-
 
 #loading unseen data
 content = "https://weather.com/weather/hourbyhour/l/c097b546627cdff2da1e276cb9b2731055718a5e7270d777a92857a9701c7870"
@@ -127,20 +129,29 @@ forecast['nameday'] = weekday
 forecast['hour'] = hour_of_day
 forecast['event_yes'] = False # This value has to be included by the user. So edit this. The value now is missing, but the model running, so even if nothing is provided, it will run
 forecast['tag_category'] = 'No event' # This value has to be included by the user. So edit this
-forecast['# of events'] = events_48['Events']
-
-
+forecast['events_count'] = events_48['Events']
 
 #making prediction on unseen data
 prediction_reg = hgr.predict(forecast)
 
 prediction_class = rfc.predict(forecast)
 
+
+def pairwise(iterable):
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = it.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
 #making a graph for the classifier
-colors = {"Low": "green", "Intermediate": "yellow", "High": "red"}
+start_time = datetime.now()
+hours_list = [start_time + timedelta(hours=x) for x in range(48)]
+
+colors = {0: "green", 1: "yellow", 2: "red"}
 category_order = ["Low", "Intermediate", "High"]
-fig_class = go.Figure(data=[go.Bar(x=forecast['hour'],
+fig_class = go.Figure(data=[go.Bar(x=hours_list,
                             y=prediction_class,
+#                             marker=dict(color='red'),
                             marker=dict(color=[colors[level] for level in prediction_class]),
                             customdata=prediction_class,
                             hovertemplate="Hour: %{x}<br>Noise Level: %{customdata}<extra></extra>")])
@@ -150,13 +161,54 @@ fig_class.update_layout(title_text="Relative Noise Levels in the Next 48 Hours")
 
 
 #making a graph for the regressor
+
+# create coordinate  pairs
+x_pairs = pairwise(time_range)
+y_pairs = pairwise(prediction_reg)
+
+# generate color list
+colors_list=[colors[level] for level in prediction_class]
+
 fig_reg = go.Figure()
-fig_reg.add_trace(data=go.Scatter(x=forecast['hour'], y=prediction_reg, name='Noise Forecast'))
-fig_reg.add_trace(go.Scatter(x=time_range, y=filtered_data['AverageNoise'], mode='lines', name='Average Noise',line=dict(dash='dash')))
+
+# add traces (line segments)
+for x, y, color in zip(x_pairs, y_pairs, colors_list):
+    fig_reg.add_trace(
+        go.Scatter(
+            x=x,
+            y=y, 
+            mode='lines', 
+            line={'color': color, 'width':7},
+        showlegend=False
+        )
+    )
+fig_reg.add_trace(go.Scatter(x=time_range, y=time_range_df['avg'], mode='lines', name='Average Noise',
+    line=dict(dash='dash', color='grey', width=3)))
 
 fig_reg.update_xaxes(title_text="Hours")
 fig_reg.update_yaxes(title_text="dB")
 fig_reg.update_layout(title_text="Noise Forecast vs Avarage for the next 48 hours")
+
+trace1 = go.Scatter(x=time_range, y=prediction_reg, name='Noise Forecast', line={'color':'blue'})
+trace2 = go.Scatter(x=time_range, y=time_range_df['avg'], mode='lines', name='Average Noise',
+    line=dict(dash='dash', color='grey', width=3))
+trace3 = go.Bar(x=hours_list,
+                            y=[65]*48,
+                            # marker=dict(color='red'),
+                            marker=dict(color=colors_list, opacity=0.2),
+                            customdata=prediction_class,
+                            hovertemplate="Hour: %{x}<br>Noise Level: %{customdata}<extra></extra>",
+                            showlegend=False)
+fig_reg_2 = go.Figure(data = [trace1, trace2, trace3])
+
+
+fig_reg_2.layout.bargap = 0.
+fig_reg_2.update_yaxes(showticklabels=True)
+fig_reg_2.update_layout(yaxis_range=[38,65])
+
+fig_reg_2.update_xaxes(title_text="Hours")
+fig_reg_2.update_yaxes(title_text="dB")
+fig_reg_2.update_layout(title_text="Noise Forecast vs Avarage for the next 48 hours")
 
 
 
